dcb01e0e1de65fb4ca19d25b3df9aea649028eb3
lordratner <49489398+lordratner@users.noreply.github.com>
2021-12-03 12:51:49 -0600

    Update gpio_rain_service.py

--- gpio_rain_service.py
+++ gpio_rain_service.py
@@ -1,4 +1,5 @@
-#You need to go into the driver for the rest of your hardware and comment out the 'Rain =' line.
+# You may need to go into the driver for the rest of your hardware and comment out the 'Rain =' line. 
+# If you are using the GW1000 driver and there is no rain sensor communicating with the GW1000, no change is needed.
 
 import syslog
 import weewx
@@ -12,24 +13,23 @@ DRIVER_VERSION = "1.0"
 
 class GpioRainGauge(StdService):
     def __init__(self, engine, config_dict):
-        super(GpioRainGauge, self).__init__(engine, config_dict)      
+        super(GpioRainGauge, self).__init__(engine, config_dict)
         d = config_dict.get('GPIORainGauge', {})
         self.gauge = RainGauge(**d)
         self.bind(weewx.NEW_LOOP_PACKET, self.load_data)
-            
+
     def load_data(self, event):
         try:
             self.get_rain(event)
         except Exception as e:
-            syslog.syslog(syslog.LOG_ERR, "GPIORainGauge: cannot read value: %s" % e) 
+            syslog.syslog(syslog.LOG_ERR, "GPIORainGauge: cannot read value: %s" % e)
 
     # Get Rainfall data
     def get_rain(self, event):
-        rainfall = round(self.gauge.get_rainfall(),2)
+        rainfall = round(self.gauge.get_rainfall(),5)
         syslog.syslog(syslog.LOG_DEBUG, "GPIORainGauge: found rain value of %s cm" % rainfall)
         event.packet['rain'] = float(rainfall)
-    
-    
+
 class RainGauge(object):
     """ Object that represents a Wired Rain Gauge. """
 
@@ -41,15 +41,14 @@ class RainGauge(object):
         # Read from config which pin to use on the RPI GPIO
         self.rain_sensor = Button(d.get('pin_number'))
         self.rain_sensor.when_pressed = self.bucket_tipped
-        
+
     def bucket_tipped(self):
         self.rain_count = self.rain_count + 1
-        
+
     def get_rainfall(self):
         rainfall = (self.rain_count * self.bucket_size)
         self.reset_rainfall()
         return rainfall
 
     def reset_rainfall(self):
-        self.rain_count = 0    
-
+        self.rain_count = 0
